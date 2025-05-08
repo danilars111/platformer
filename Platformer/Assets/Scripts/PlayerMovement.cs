b0b305d168ba49e6e115901f607d2f8954637117
Daniel <daniellarsson054@gmail.com>
2025-05-08 20:20:42 +0200

    Add Player Sprite and animations
    Jump, run and idle animations for player

--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,58 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] 
-    private float speed;
+    private float _speed = 5;
     
-    private Rigidbody2D body;
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private int _direction = 1;
+
+    private bool _grounded;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+       
     }
 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocityY);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // -1 on x-axis scale flips character
 
-        if(Input.GetKey(KeyCode.Space))
+        if ((horizontalInput > 0 && transform.localScale.x < 0) || 
+            (horizontalInput < 0 && transform.localScale.x > 0))
         {
-            body.linearVelocity = new Vector2(body.linearVelocityX, speed);
+            _direction *= -1;
+            transform.localScale = new Vector3(_direction, 1, 1);
+        }
+
+        _body.linearVelocity = new Vector2(horizontalInput * _speed, _body.linearVelocityY);
+
+
+        if (Input.GetKey(KeyCode.Space) && _grounded)
+        {
+            Jump();
+        }
+
+        _anim.SetBool("run", horizontalInput != 0);
+        _anim.SetBool("grounded", _grounded);
+    }
+
+    private void Jump()
+    {
+        _grounded = false;
+        _anim.SetTrigger("jump");
+        _body.linearVelocity = new Vector2(_body.linearVelocityX, _speed);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            _grounded = true;
         }
     }
 }
